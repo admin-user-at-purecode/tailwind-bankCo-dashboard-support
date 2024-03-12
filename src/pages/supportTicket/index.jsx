9bda26bd52aa406b4c0d217473d413ca9d9a4831
diksha2150 <112609437+diksha2150@users.noreply.github.com>
2024-03-12 12:02:34 +0530

    added screenshots and varrient

--- src/pages/supportTicket/index.jsx
+++ src/pages/supportTicket/index.jsx
@@ -3,7 +3,6 @@ import Note from "../../component/support/Note";
 import PresentAddress from "../../component/support/PresentAddress";
 import ShippingAddress from "../../component/support/ShippingAddress";
 import ContactInfo from "../../component/support/ContactInfo";
-import CustomerInfo from "../../component/support/CustomerInfo";
 import RecentSupport from "../../component/support/RecentSupport";
 import SupportTicket from "../../component/forms/SupportTicket";
 import SupportHeader from "../../component/support/Header";
@@ -29,7 +28,7 @@ function Support() {
         <div className="w-full 2xl:col-span-3 col-span-4 space-y-10">
           <div className="bg-white dark:bg-darkblack-600 dark:border-darkblack-400 p-8 divide-y divide-bgray-300 dark:divide-darkblack-400 rounded-lg">
             {/* Customer  */}
-            <CustomerInfo />
+
             {/* Contact Info  */}
             <ContactInfo />
             {/* Shipping Address */}
